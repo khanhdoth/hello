3a1959604e1d81fad96a48491947627a8a84676f
khanhdoth <khanh.doth@gmail.com>
2020-05-09 15:58:14 +0000

    u

---
+++ lib/hello_web/controllers/hello_controller.ex
@@ -0,0 +1,14 @@
+defmodule HelloWeb.HelloController do
+  use HelloWeb, :controller
+
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+
+  def show(conn, %{"messenger" => messenger}) do
+    render(conn, "show.html", messenger: messenger)
+    #conn
+    #|> assign(:messenger, messenger)
+    #|> render("show.html")
+  end
+end
